22f875b545a5353dca4b580c5bbfda1cb297e825
Gabriela Colla <51380540+gabrielacolla@users.noreply.github.com>
2021-09-30 21:29:48 -0300

    update enemy

--- projeto-3d-faj/Assets/Scripts/enemyai.cs
+++ projeto-3d-faj/Assets/Scripts/enemyai.cs
@@ -12,6 +12,8 @@ public class enemyai : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
+    public GameObject projectile;
+
     // Patrulha
     public Vector3 walkPoint;
     bool walkPointSet;
@@ -27,10 +29,70 @@ public class enemyai : MonoBehaviour
 
     private void Awake()
     {
-        player = GameObject.Find()
+        player = GameObject.Find("Enemy").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
+    private void Update()
+    {
+        playerInsightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerIntAttackRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
 
+        if (!playerInsightRange && !playerIntAttackRange) Patrulhar();
+        if (playerInsightRange && !playerIntAttackRange) PerseguirPlayer();
+        if (playerIntAttackRange && playerInsightRange) AttackPlayer();
 
+    }
+
+    private void Patrulhar()
+    {
+        if (!walkPointSet) SearchwalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanteToWalkPoint = transform.position - walkPoint;
+
+        if (distanteToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchwalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomx = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomx, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up))
+            walkPointSet = true;
+    }
+    private void PerseguirPlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
 
 }
